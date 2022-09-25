4213ae834cd138f13c5fb47f72e3cb8b4cf502f0
Ana Anaya <anaya.anap@gmail.com>
2022-09-24 19:17:44 -0700

    add button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import freecodecamplogo from './imagenes/freecodecamp-logo.png'
+import Boton from './componentes/Boton';
 
 function App() {
+
+  const manejarClick = () => {
+    console.log('Click');
+  }
+  const reiniciarcontador = () => {
+    console.log('Reiniciar');
+  }
+
   return (
     <div className="App">
-
+      <div className="freecodecamp-logo-contenedor">
+        <img
+        className = 'freecodecampo-logo'
+        src = {freecodecamplogo}
+        alt = 'Logo de freecodecamp'/>
+      </div>
+      <div className = 'contenedor-principal'>
+        <Boton
+          texto = 'Click'
+          esBotondeClick= {true}
+          manejarClick= {manejarClick}/>
+        <Boton
+          texto= 'Reiniciar'
+          esBotondeClick= {false}
+          manejarClick= {reiniciarcontador}/>
+      </div>
     </div>
   );
 }
